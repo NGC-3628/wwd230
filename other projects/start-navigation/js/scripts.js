a7dbc3e801311243421260bb84ff9bf5e40fd6f3
isaacvgn@gmail.com <itu22002@byui.edu>
2023-05-14 19:07:16 -0600

    W4.1

---
+++ other projects/start-navigation/js/scripts.js
@@ -0,0 +1,9 @@
+function toggledMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggledMenu;
+
+
