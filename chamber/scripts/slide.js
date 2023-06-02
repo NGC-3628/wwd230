6bfb54fa1b90a9ab34aa8d57e8c8c5ebf8550caa
isaacvgn@gmail.com <itu22002@byui.edu>
2023-06-02 09:03:48 -0600

    W6.1

---
+++ chamber/scripts/slide.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(".slider-1").slick({
+      autoplay: true,
+      autoplaySpeed: 3200,
+      dots: true,
+      arrows: false
+    });
+  });
+  
+
+  $(document).ready(function() {
+    $(".slider-2").slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      dots: true,
+      arrows: false
+    });
+  });
+  
