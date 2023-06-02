f84863520adefd9873627199219b6e44153df5c0
isaacvgn@gmail.com <itu22002@byui.edu>
2023-06-02 09:39:11 -0600

    W6.z

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,9 +1,15 @@
-let t = parseFloat(document.querySelector(".temperature").innerHTML)
-let s = parseFloat(document.querySelector(".wind-speed").innerHTML)
-let chillFactor = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))
-if (t <= 50.0 && s > 3.0) {
-    document.querySelector(".wind-chill").innerHTML = Math.ceil(chillFactor)
-}
-else {
-    document.querySelector(".colored.chill").innerHTML = "N/A"
+// Get the temperature and wind speed values from the page
+var temperatureValue = parseFloat(document.getElementById("temperature").innerText);
+var windSpeedValue = parseFloat(document.getElementById("wind-speed-value").innerText);
+
+// Check if the values meet the specification limits
+if (temperatureValue <= 50 && windSpeedValue > 3.0) {
+  // Calculate the wind chill factor
+  var windChillValue = 35.74 + (0.6215 * temperatureValue) - (35.75 * Math.pow(windSpeedValue, 0.16)) + (0.4275 * temperatureValue * Math.pow(windSpeedValue, 0.16));
+
+  // Display the wind chill factor value
+  document.getElementById("wind-chill-value").innerText = windChillValue.toFixed(2) + " °F";
+} else {
+  // Display "N/A" if the values do not meet the requirements
+  document.getElementById("wind-chill-value").innerText = "N/A";
 }
