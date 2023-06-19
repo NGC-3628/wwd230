c6d88c1eca6ef59ae5088a03c5461d5a7877541a
isaacvgn@gmail.com <itu22002@byui.edu>
2023-06-18 20:32:16 -0600

    W9.u

---
+++ chamber/scripts/direcotry.js
@@ -0,0 +1,48 @@
+//JSON link
+const url = "https://github.com/NGC-3628/wwd230/blob/main/lesson%209/companies.json";
+
+
+// Add event listeners to filters
+document.addEventListener("DOMContentLoaded", () => {
+    // select the output container element
+    const cards = document.querySelector('div.cards'); 
+    // No filter button
+    document.querySelector("#noFilter").addEventListener("click", () => {
+        // Clear cards
+        cards.innerHTML = "";
+        // Get prophets data and display
+        getProphetData().then(companies => displayProphets(companies));
+    });
+
+     // Served more than a decade filter
+     document.querySelector("#decade-filter").addEventListener("click", () => {
+        // Clear cards
+        cards.innerHTML = "";
+        // Get prophets data
+        getProphetData().then(companies => {
+            // filter by years of service
+            const filter = companies.filter((company) => {
+                // Only more than ten years
+                if (company.length >= 10) {return company;}
+            });
+            // Display prophets
+            displayProphets(filter);
+        })
+    });
+    // Get prophets data and display
+    getCompanytData().then(companies => displayProphets(companies));
+});
+
+/**
+ * Gets the company data from a JSON request
+ */
+async function getCompanyData() {
+    // Fetch data from url and wait for response
+    const response = await fetch(url);
+    // Get JSON data from response
+    const data = await response.json();
+    // Output the Array data from the JSON
+    //console.table(data.prophets); 
+    // Return prophets data
+    return data.companies;
+}
