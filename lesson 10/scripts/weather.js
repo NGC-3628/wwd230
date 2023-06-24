cb1787477477c12ae1919f2d19cef77234ea4f3b
isaacvgn@gmail.com <itu22002@byui.edu>
2023-06-24 15:26:03 -0600

    W10.f

---
+++ lesson 10/scripts/weather.js
@@ -0,0 +1,35 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Torreon&units=imperial&appid=d297df6b49870f2c7ec944780213cbca"
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)} &deg;F</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+
+}
+apiFetch();
