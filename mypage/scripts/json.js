d31aff45fba2d50c1e8d338e321b29c1f2fe1c13
isaacvgn@gmail.com <itu22002@byui.edu>
2023-07-12 00:27:52 -0600

    W13.m

--- mypage/scripts/json.js
+++ mypage/scripts/json.js
@@ -1,60 +1,65 @@
-//JSON link
+// JSON link
 const url = "https://ngc-3628.github.io/wwd230/other%20projects/customers.json";
 
-//add event listener to load data and display:
+// Add event listener to load data and display
 document.addEventListener("DOMContentLoaded", () => {
-    
-    //ouptup to display info on:
-    const cards = document.querySelector("div.cust-clien");
+  // Output to display info on
+  const carouselInner = document.querySelector(".carousel-inner");
 
-    //Fetch data and display it:
-    getCompanyData().then(companies => displayCompanies(companies));
+  // Fetch data and display it
+  getCompanyData().then(companies => displayCompanies(companies, carouselInner));
 });
 
 /**
- * fetch the company data from the JSON file
- * 
+ * Fetch the company data from the JSON file
  */
-async function getCompanyData(){
-    //Fetch dara from teh provided URL and wait for its response
+async function getCompanyData() {
+  try {
+    // Fetch data from the provided URL and wait for its response
     const response = await fetch(url);
     // Get JSON data from the response
     const data = await response.json();
-    //Return the array of company data
-    return data.companies
+    // Return the array of company data
+    return data.companies;
+  } catch (error) {
+    console.error("Error fetching company data:", error);
+    return []; // Return an empty array in case of error
+  }
 }
 
 /**
- * Display company data on the web page
+ * Display company data in the carousel
  * @param {Array} companies Array of company data
+ * @param {HTMLElement} carouselInner Container for carousel items
  */
-function displayCompanies(companies) {
-    //select the output container element
-    const cards = document.querySelector("div.cust-clien");
+function displayCompanies(companies, carouselInner) {
+  companies.forEach(company => {
+    const card = document.createElement("div");
+    card.classList.add("company-card");
 
-    //loop thru every company
-    companies.forEach(company => {
-        //create elements for company information
-        let logo = document.createElement("img");
-        let name = document.createElement("p");
+    const logo = document.createElement("img");
+    logo.setAttribute("src", company.imageurl);
+    logo.classList.add("customers-logo");
 
-        //set contant for company information
-        logo.setAttribute("src", company.imageurl);
-        name.textContent = company.name;
+    const name = document.createElement("p");
+    name.textContent = company.name;
+    name.classList.add("customers-name");
 
-        //Append the elements to the cards
-        cards.appendChild(logo);
-        cards.appendChild(name);
+    card.appendChild(logo);
+    card.appendChild(name);
+    carouselInner.appendChild(card);
+  });
 
-        //Making elements plastic thus CSS affect 'em
-        logo.setAttribute("class", "customers-logo");
-        name.setAttribute("class", "customers-name");
+  // Add event listeners to the carousel controls
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const carousel = document.querySelector(".carousel");
 
+  prevBtn.addEventListener("click", () => {
+    carousel.style.transform = "translateX(100%)";
+  });
 
-        //Add CSS class to card for styling
-        cards.classList.add("company-card");
-
-        //Append the card to the container
-        cards.appendChild(card);
-    })
+  nextBtn.addEventListener("click", () => {
+    carousel.style.transform = "translateX(-100%)";
+  });
 }
