e6bffc2025706d0383436b4c0bee2c4ece0f05a8
isaacvgn@gmail.com <itu22002@byui.edu>
2023-07-15 23:51:57 -0600

    W13.a

--- mypage/scripts/script.js
+++ mypage/scripts/script.js
@@ -1,8 +1,7 @@
+
 function toggledMenu() {
-    document.getElementById("menu").classList.toggle("open");
-    document.getElementById("hamburger").classList.toggle("open");
-  }
-  
-  const hamburger = document.getElementById("hamburger");
-  hamburger.addEventListener("click", toggledMenu);
-  
+    document.querySelector("nav ul#menu").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburger");
+x.onclick = toggledMenu;
